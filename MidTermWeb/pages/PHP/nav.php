3be783174327cb229ece0dfc67f324270771d5b3
TanaJohn <54981076+9Johnfon@users.noreply.github.com>
2024-03-19 20:17:53 +0700

    Add files via upload

---
+++ MidTermWeb/pages/PHP/nav.php
@@ -0,0 +1,11 @@
+
+<div class="nav-menu">
+        <div class="menu-toggle" onclick="toggleMenu()">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        <ul class="nav-list">
+            <li><a href="./index.html">Cannabis</a></li>
+        </ul>
+    </div>
